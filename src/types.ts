9834ff5bdfdd91bf7a170bc5d8dbe116bed9a705
Roc Wu <rocwind@gmail.com>
2021-03-10 10:32:28 +0800

    feat(types): add ParametersTypes<> and Promisify<>

--- src/types.ts
+++ src/types.ts
@@ -27,8 +27,18 @@ export type PropertyType<T extends object, K extends keyof T> = T[K];
 export type ArrayItemType<T extends unknown[]> = T[number];
 
 /**
+ * Extract thee types array of function parameters
+ */
+export type ParametersTypes<T> = T extends (...args: infer P) => unknown ? P : [];
+
+/**
  * Convert interface type to type, it can be used to work with Record<string, unknown>
  * check out this thread for details
  * https://github.com/microsoft/TypeScript/issues/15300#issuecomment-760165845
  */
 export type Typify<T> = { [K in keyof T]: T[K] };
+
+/**
+ * Covert type T to Promise<T> except for Promises
+ */
+export type Promisify<T> = T extends Promise<unknown> ? T : Promise<T>;
